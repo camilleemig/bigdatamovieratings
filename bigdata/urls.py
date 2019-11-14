0faa557eb35d6c49a5e46d437446e459c3051377
RachelHamilton <rachel13hamilton@gmail.com>
2019-11-14 14:09:04 -0500

    login

--- bigdata/urls.py
+++ bigdata/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ratings/', include('ratings.urls')),
-
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
