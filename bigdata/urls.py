ec46e902014d683400fda582aab52df20058ee04
RachelHamilton <rachel13hamilton@gmail.com>
2019-11-14 16:21:31 -0500

    auth

--- bigdata/urls.py
+++ bigdata/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
