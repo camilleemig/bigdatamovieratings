ed16e49e9f2aaa3a90ad9c3f4ba33c4e4ad9ebd0
Camille Emig <camille.emig@gmail.com>
2019-11-14 15:24:56 -0500

    Fixing movies link to be movies

--- ratings/urls.py
+++ ratings/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('movies/', views.categories, name='movies'),
+    path('movies/', views.movies, name='movies'),
     path('categories/', views.categories, name='categories'),
     path('<int:movie_id>/', views.movie, name='movie'),
     path('<str:category>/', views.category, name='category'),
