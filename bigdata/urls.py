f8f3a3bbcdbfc58c6c51ba1f0bc7e24974b696b4
RachelHamilton <rachel13hamilton@gmail.com>
2019-11-26 10:45:58 -0500

    Update urls.py

--- bigdata/urls.py
+++ bigdata/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth.models import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
